46e9b2443a3823d27de36db8336df637372d9877
Dejan Trajkovic <dtrajko@gmail.com>
2020-08-23 06:06:29 +0200

    72. Making a New C++ Project in Hazel (part 2)

--- Hazel-Editor/src/EditorLayer.cpp
+++ Hazel-Editor/src/EditorLayer.cpp
@@ -10,7 +10,7 @@
 namespace Hazel {
 
 	EditorLayer::EditorLayer()
-		: Layer("EditorLayer"), m_CameraController(16.f / 9.f, true)
+		: Layer("EditorLayer"), m_CameraController(16.f / 9.f), m_SquareColor({ 0.2f, 0.3f, 0.8f, 1.0f })
 	{
 		Hazel::RendererAPI::SetMode(Hazel::RendererAPI::Mode::Renderer2D);
 	}
@@ -22,6 +22,11 @@ namespace Hazel {
 		m_CameraController.SetZoomLevel(6.5f);
 
 		m_CheckerboardTexture = Hazel::Texture2D::Create("assets/textures/Checkerboard.png");
+
+		FramebufferSpecification fbSpec;
+		fbSpec.Width = 1280;
+		fbSpec.Height = 720;
+		m_Framebuffer = Framebuffer::Create(fbSpec);
 	}
 
 	void EditorLayer::OnDetach()
@@ -29,12 +34,12 @@ namespace Hazel {
 		HZ_PROFILE_FUNCTION();
 	}
 
-	void EditorLayer::OnUpdate(Hazel::Timestep timestep)
+	void EditorLayer::OnUpdate(Hazel::Timestep ts)
 	{
 		HZ_PROFILE_FUNCTION();
 
 		// Update
-		m_CameraController.OnUpdate(timestep);
+		m_CameraController.OnUpdate(ts);
 
 		// Render
 		Hazel::Renderer2D::ResetStats();
@@ -43,6 +48,34 @@ namespace Hazel {
 			HZ_PROFILE_SCOPE("Renderer Prep");
 			Hazel::RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1 });
 			Hazel::RenderCommand::Clear();
+		}
+
+		{
+			static float rotation = 0.0f;
+			rotation += ts = 10.0f;
+
+			HZ_PROFILE_SCOPE("Renderer Draw");
+			Hazel::Renderer2D::BeginScene(m_CameraController.GetCamera());
+
+			Hazel::Renderer2D::DrawRotatedQuad({ 0.6f, -0.4f }, { 0.8f, 0.8f }, glm::radians(rotation), { 0.8f, 0.2f, 0.3f, 1.0f });
+			Hazel::Renderer2D::DrawQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, { 0.8f, 0.2f, 0.3f, 1.0f });
+			Hazel::Renderer2D::DrawQuad({ 0.8f,  0.4f }, { 0.5f, 0.75f }, { 0.2f, 0.3f, 0.8f, 1.0f });
+			Hazel::Renderer2D::DrawQuad({ 0.0f, -0.3f }, { 0.5f, 0.5f }, { 1.0f, 1.0f, 0.0f, 1.0f });
+			Hazel::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckerboardTexture, 10.0f);
+			Hazel::Renderer2D::DrawRotatedQuad({ 0.0f, 0.0f,  0.0f }, { 3.0f, 3.0f }, glm::radians(45.0f), m_CheckerboardTexture, 10.0f);
+			Hazel::Renderer2D::EndScene();
+
+			Hazel::Renderer2D::BeginScene(m_CameraController.GetCamera());
+			for (float y = -5.0f; y <= 5.0f; y += 0.25f)
+			{
+				for (float x = -5.0f; x <= 5.0f; x += 0.25f)
+				{
+					glm::vec4 color = { (x + 5.0f) / 10.0f, 0.4f, (y + 5.0f) / 10.0f, 1.0f };
+					Hazel::Renderer2D::DrawQuad({ x, y }, { 0.23f, 0.23f }, color);
+				}
+			}
+
+			Hazel::Renderer2D::EndScene();
 		}
 
 		if (Hazel::Input::IsMouseButtonPressed(HZ_MOUSE_BUTTON_LEFT))
