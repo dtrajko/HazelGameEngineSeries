ba96ca2b889b146b944285a2f632cdea3df6d3ed
Dejan Trajkovic <dtrajko@gmail.com>
2020-09-21 00:16:37 +0200

    78. Entities and Components (debug)

--- Hazel/src/Hazel/Scene/Scene.cpp
+++ Hazel/src/Hazel/Scene/Scene.cpp
@@ -20,6 +20,9 @@ namespace Hazel {
 	{
 		struct MeshComponent
 		{
+			bool Data;
+
+			MeshComponent() = default;
 		};
 
 		struct TransformComponent
@@ -40,29 +43,29 @@ namespace Hazel {
 		DoMath(transform);
 
 		entt::entity entity = m_Registry.create();
-		// m_Registry.emplace<TransformComponent>(entity, glm::mat4(1.0f));
+		//	m_Registry.emplace<TransformComponent>(entity, glm::mat4(1.0f));
 
-		m_Registry.on_construct<TransformComponent>().connect<&OnTransformConstruct>();
+		//	m_Registry.on_construct<TransformComponent>().connect<&OnTransformConstruct>();
 
 		if (m_Registry.has<TransformComponent>(entity)) {
-			// TransformComponent& transform = m_Registry.get<TransformComponent>(entity);
+			//	TransformComponent& transform = m_Registry.get<TransformComponent>(entity);
 		}
 
-		//	auto view = m_Registry.view<TransformComponent>();
-		//	for (auto entity : view)
-		//	{
+		// auto view = m_Registry.view<TransformComponent>();
+		// for (auto it = view.begin(); it != view.end(); it++)
+		// {
 		//		auto component = view.get<TransformComponent>(entity);
 		//		TransformComponent& transform = view.get<TransformComponent>(entity);
-		//	}
+		// }
 
-		//	auto group = m_Registry.group<TransformComponent>(entt::get<MeshComponent>);
-		//	for (auto entity : group)
-		//	{
+		auto group = m_Registry.group<TransformComponent>(entt::get<MeshComponent>);
+		for (auto it = group.begin(); it != group.end(); it++)
+		{
 		//		auto component = group.get<TransformComponent>(entity);
-		//		// auto&[transform, mesh] = group.get<TransformComponent, MeshComponent>(entity);
-		//	
-		//		// Renderer::Submit(mesh, transform);
-		//	}
+		//		auto&[transform, mesh] = group.get<TransformComponent, MeshComponent>(entity);
+		//
+		//		Renderer::Submit(mesh, transform);
+		}
 
 		//	m_Registry.remove();
 		//	m_Registry.clear();
