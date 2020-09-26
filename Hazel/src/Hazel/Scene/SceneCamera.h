a7f49ac9757b173a55c771c3ff3b3dec214b9080
Dejan Trajkovic <dtrajko@gmail.com>
2020-09-26 22:33:01 +0200

    86. Camera Component UI (ortho)

--- Hazel/src/Hazel/Scene/SceneCamera.h
+++ Hazel/src/Hazel/Scene/SceneCamera.h
@@ -15,10 +15,18 @@ namespace Hazel {
 
 		void SetOrthographic(float size, float nearClip, float farClip);
 		void SetViewportSize(uint32_t width, uint32_t height);
+
 		float GetOrthographicSize() const { return m_OrthographicSize; }
 		void SetOrthographicSize(float size) { m_OrthographicSize = size; RecalculateProjection(); }
 
+		float GetOrthographicNearClip() const { return m_OrthographicNear; };
+		void SetOrthographicNearClip(float nearClip) { m_OrthographicNear = nearClip; RecalculateProjection(); };
+
+		float GetOrthographicFarClip() const { return m_OrthographicFar; };
+		void SetOrthographicFarClip(float farClip) { m_OrthographicFar = farClip; RecalculateProjection(); };
+
 		ProjectionType GetProjectionType() const { return m_ProjectionType; };
+		void SetProjectionType(ProjectionType type) { m_ProjectionType = type; RecalculateProjection(); };
 
 	private:
 		void RecalculateProjection();
--- Hazelnut/src/Panels/SceneHierarchyPanel.cpp
+++ Hazelnut/src/Panels/SceneHierarchyPanel.cpp
@@ -106,13 +106,45 @@ namespace Hazel
 			if (ImGui::TreeNodeEx((void*)typeid(CameraComponent).hash_code(), ImGuiTreeNodeFlags_DefaultOpen, "Camera"))
 			{
 				auto& cameraComponent = entity.GetComponent<CameraComponent>();
+				auto& camera = cameraComponent.Camera;
 
 				const char* projectionTypeStrings[] = { "Perspective", "Orthographic" };
-				const char* currentProjectionTypeString = projectionTypeStrings[(int)cameraComponent.Camera.GetProjectionType()];
+				const char* currentProjectionTypeString = projectionTypeStrings[(int)camera.GetProjectionType()];
 
 				if (ImGui::BeginCombo("Projection", currentProjectionTypeString))
 				{
+					for (int i = 0; i < 2; i++)
+					{
+						bool isSelected = currentProjectionTypeString == projectionTypeStrings[i];
+						if (ImGui::Selectable(projectionTypeStrings[i], isSelected))
+						{
+							currentProjectionTypeString = projectionTypeStrings[i];
+							camera.SetProjectionType((SceneCamera::ProjectionType)i);
+						}
+
+						if (isSelected)
+							ImGui::SetItemDefaultFocus();
+					}
 					ImGui::EndCombo();
+				}
+
+				if (camera.GetProjectionType() == SceneCamera::ProjectionType::Perspective)
+				{
+				}
+
+				if (camera.GetProjectionType() == SceneCamera::ProjectionType::Orthographic)
+				{
+					float orthoSize = camera.GetOrthographicSize();
+					if (ImGui::DragFloat("Size", &orthoSize))
+						camera.SetOrthographicSize(orthoSize);
+
+					float orthoNear = camera.GetOrthographicNearClip();
+					if (ImGui::DragFloat("Near", &orthoNear))
+						camera.SetOrthographicNearClip(orthoNear);
+
+					float orthoFar = camera.GetOrthographicFarClip();
+					if (ImGui::DragFloat("Far", &orthoFar))
+						camera.SetOrthographicFarClip(orthoFar);
 				}
 
 				ImGui::TreePop();
