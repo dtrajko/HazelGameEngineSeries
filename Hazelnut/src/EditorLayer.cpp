75f542d6f5fad3a5187962f5ecd1991f0f168dc2
Dejan Trajkovic <dtrajko@gmail.com>
2021-02-28 23:15:50 +0100

    99. Clicking to Select Entities

--- Hazelnut/src/EditorLayer.cpp
+++ Hazelnut/src/EditorLayer.cpp
@@ -157,13 +157,9 @@ namespace Hazel {
 
 		if (mouseX >= 0 && mouseY >= 0 && mouseX < (int)viewportSize.x && mouseY < (int)viewportSize.y)
 		{
-			// int pixel = m_ActiveScene->Pixel(mouseX, mouseY);
 			int pixelData = m_Framebuffer->ReadPixel(1, mouseX, mouseY);
 			m_HoveredEntity = pixelData == -1 ? Entity() : Entity((entt::entity)pixelData, m_ActiveScene.get());
 		}
-
-		// m_ActiveScene->DrawIDBuffer(m_IDFramebuffer, m_EditorCamera);
-		// m_ActiveScene->OnUpdateRuntime(ts);
 
 		m_Framebuffer->Unbind();
 	}
